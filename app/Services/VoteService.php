94e44d1f2304de4deaed48aa2d3ae3f767eb7f62
MariaE003 <elgotbymaria@gmail.com>
2026-04-16 21:48:56 +0100

    ajout VoteService avec gestion des votes de base

---
+++ app/Services/VoteService.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Services;
+
+use App\Repositories\VoteRepository;
+
+class VoteService{
+   protected $voteRepo;
+
+   public function __construct(VoteRepository $voteRepo){
+        $this->voteRepo=$voteRepo;
+   }
+   public function allVotes(){
+        return $this->voteRepo->getAll();
+   }
+   public function createVote($data){
+        return $this->voteRepo->create($data);
+   }
+   public function deleteVote($id){
+        $this->voteRepo->remove($id);
+   }
+   
+}
+
+?>
