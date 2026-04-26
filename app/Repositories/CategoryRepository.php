b8e3ffcc168334a3b2ee1a7448c39d0ce6f097b3
MariaE003 <elgotbymaria@gmail.com>
2026-04-26 22:06:42 +0100

    ajout methodes findById update et delete dans repository categorie

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -10,6 +10,19 @@ class CategoryRepository{
     public function create($data){
         return Category::create($data);
     }
+    public function findById($id){
+        return Category::findOrFail($id);
+    }
+    public function update($id, $data){
+        $category = Category::findOrFail($id);
+        $category->update($data);
+        return $category;
+    }
+
+    public function delete($id){
+        $category = Category::findOrFail($id);
+        return $category->delete();
+    }
 }
 
 ?>
