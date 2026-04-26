5dae5be0c032ca3464f3fd833c9cde7a7fbdbc7f
MariaE003 <elgotbymaria@gmail.com>
2026-04-26 22:08:24 +0100

    ajout methodes get update et delete pour categorie dans service

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -4,16 +4,25 @@ namespace App\Services;
 use App\Repositories\CategoryRepository;
 
 class CategoryService{
-   protected $categorieRepo;
-   public function __construct(CategoryRepository $categorieRepo){
-        $this->categorieRepo=$categorieRepo;
-   }
-   public function allCategories(){
-        return $this->categorieRepo->getAll();
-   }
-   public function createCategory($data){
-        return $this->categorieRepo->create($data);
-   }
+     protected $categorieRepo;
+     public function __construct(CategoryRepository $categorieRepo){
+          $this->categorieRepo=$categorieRepo;
+     }
+     public function allCategories(){
+          return $this->categorieRepo->getAll();
+     }
+     public function createCategory($data){
+          return $this->categorieRepo->create($data);
+     }
+     public function getCategoryById($id){
+          return $this->categorieRepo->findById($id);
+     }
+     public function updateCategory($id, $data){
+          return $this->categorieRepo->update($id, $data);
+     }
+     public function deleteCategory($id){
+          return $this->categorieRepo->delete($id);
+     }
 }
 
 ?>
